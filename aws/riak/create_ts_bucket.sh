8473b8e9e7f5b8a0702473a3ff68019dfbd68dc7
Rob Genova <rcgenova@gmail.com>
2016-03-07 13:35:42 -0800

    remove TS smoke test scripts

--- aws/riak/create_ts_bucket.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Creating bucket $1..."
-
-sudo riak-admin bucket-type create $1 '{"props":{"table_def": "CREATE TABLE '"$1"' (myfamily varchar not null, myseries varchar not null, time timestamp not null, weather varchar not null, temperature double, PRIMARY KEY ((myfamily, myseries, quantum(time, 2, 'h')), myfamily, myseries, time))"}}'
-
-sudo riak-admin bucket-type activate $1
