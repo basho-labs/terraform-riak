a6ce218f4ef3b95268a38046d65fc28c6a0785ab
Rob Genova <rcgenova@gmail.com>
2016-03-25 16:42:06 -0700

    Improve Vagrant support

--- Vagrantfile
+++ Vagrantfile
@@ -3,6 +3,8 @@
 
 Vagrant.configure(2) do |config|
 
+  config.env.enable
+
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
   config.vm.box = "centos/7"
@@ -11,11 +13,10 @@ Vagrant.configure(2) do |config|
   # using a specific IP.
   # config.vm.network "private_network", ip: "192.168.33.10"
 
-  # Enable provisioning with a shell script. Additional provisioners such as
-  # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
-  # documentation for more information about their specific syntax and use.
-  config.vm.provision "shell", path: "setup.sh"
-
   config.vm.provision "file", source: "aws", destination: "/home/vagrant"
+  config.vm.provision "file", source: ".env", destination: "/home/vagrant/aws.conf"
+  config.vm.provision "shell", path: "setup.sh"
+  file_name = File.basename ENV['KEY_PATH']
+  config.vm.provision "file", source: ENV['KEY_PATH'], destination: "/home/vagrant/.ssh/" + file_name
 
 end
--- setup.sh
+++ setup.sh
@@ -13,4 +13,26 @@ unzip terraform_0.6.8_linux_amd64.zip
 echo "Installing Ansible..."
 git clone git://github.com/ansible/ansible.git --recursive
 
+echo "Updating AWS configs..."
+KEY_PATH=$(sed '1q;d' aws.conf | cut -d '=' -f 2)
+KEY_NAME=$(basename $KEY_PATH)
+KEY_NAME=${KEY_NAME%.*}
+AWS_ACCESS_KEY=$(sed '2q;d' aws.conf | cut -d '=' -f 2)
+AWS_SECRET_KEY=$(sed '3q;d' aws.conf | cut -d '=' -f 2)
+
+sed -i "47s/.*/    default = \"$KEY_NAME\"/" aws/global.tf
+sed -i "52s~.*~    default = \"/home/vagrant/.ssh/$KEY_NAME\"~" aws/global.tf
+sed -i "57s/.*/    default = \"$AWS_ACCESS_KEY\"/" aws/global.tf
+sed -i "62s~.*~    default = \"$AWS_SECRET_KEY\"~" aws/global.tf
+
+rm aws.conf
+
+# Recreate symlinks
+rm aws/riak/global_variables.tf
+rm aws/clients/global_variables.tf
+rm aws/security/global_variables.tf
+ln -s ../global.tf aws/riak/global_variables.tf
+ln -s ../global.tf aws/clients/global_variables.tf
+ln -s ../global.tf aws/security/global_variables.tf
+
 echo 'export PATH="$PATH":/home/vagrant' | tee --append /home/vagrant/.bashrc
